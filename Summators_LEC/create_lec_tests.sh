e2c7c034a2a1e763a5b20572d3bfe859fb033559
Vikseko <vikseko@gmail.com>
2024-01-05 16:55:33 +0800

    Create folders

--- Summators_LEC/create_lec_tests.sh
+++ Summators_LEC/create_lec_tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mkdir -p cnfs
+
 python3 ../Scripts/LEC_by_two_circuits.py -nf ../Summators/aags/SumSimple$1+$2.aag -ns ../Summators/aags/SumLogHeight$1+$2.aag -o ../Summators_LEC/cnfs/lec_sum_SvLH_$1x$2.cnf
